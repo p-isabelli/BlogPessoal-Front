36cd658578fbb5e99d9ff491be8f443c47d5e7c6
isabelli c pereira <isabellipereira58@gmail.com>
2024-02-21 22:23:43 -0300

    criando models

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import './App.css';
-import Home from './paginas/home/Home';
-import Navbar from './components/navBar/NavBar';
-import Footer from './components/footer/Footer';
+import UserProvider from './contexts/UserContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 
+
 function App() {
+
   return (
-    <>
-    <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-);
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+
+      </BrowserRouter>
+
+    </UserProvider>
+  );
 }
+
 export default App;
